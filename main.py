1738c6a29450224afbc08ad5af9802c872aa4038
Taperofpaper <AidanCullen200@gmail.com>
2022-09-17 15:14:19 -0400

    made notification msg vary depending on moisture level of sensor

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ client = Client("AC337e1f870f1ce671c5a17f77cdec2f94", "4b028e7d594418d5e74192918
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
-client.messages.create(to="+16047720899",
-                       from_="+19014459525",
-                       body="Hello from Python!")
+def send_watering_msg(moisture):
+    text = "Your plant needs watering: the soil is "
+
+    # display the appropriate message based on soil moisture
+    # note: moisture levels for each need to be changed at the end to reflect sensor data
+    if moisture == 1:
+        text += "mildly dry."
+    elif moisture == 2:
+        text += "moderately dry."
+    else:
+        text += "very dry."
+
+    client.messages.create(to="+16047720899",
+                           from_="+19014459525",
+                           body=text)
